cee605f2b92ae6b25a5bc1d48a88ed451671ad85
Jeandre <jeandreross@gmail.com>
2021-08-13 16:32:41 +0200

    update

--- src/view-products.js
+++ src/view-products.js
@@ -1,5 +1,6 @@
 let base_URL = "https://lca-pointofsales.herokuapp.com//view-products";
 
+// view product function
 function getProducts(url) {
   fetch(url)
     .then((response) => response.json())
@@ -15,11 +16,12 @@ function getProducts(url) {
 
       products.forEach((product) => {
           container.innerHTML += `<div class='products-container'><img src='${ product[5] }' class='productImage'></img>
-          <div class='productName'>${product[1]}</div>
-          <div class='productCategory'>${product[2]}</div>
-          <div class='productPrice'>R${product[3]}</div>
-          <div>${product[4]}</div><div>
-          <button>Add To Cart</button>`
+          <div class="productID">Product ID: ${ product[0] }</div>
+          <div class='productName'>Product Name: ${product[1]}</div>
+          <div class='productCategory'>Category: ${product[2]}</div>
+          <div class='productPrice'>Price: R${product[3]}</div>
+          <div class="productDescription"><p>Product Description:</p> ${product[4]}</div>
+          <div class="button"><button class="btn">Add To Cart</button> <input type="number" id="quantity" name="quantity" min="1" max="5"></div>`
       });
     });
 }
