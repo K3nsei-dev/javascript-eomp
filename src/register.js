6112d6ef48f0b28cfd2734624dbf3f9ef4d5de6d
Jeandre Awood <jeandreross@gmail.com>
2021-08-17 22:30:37 +0200

    update

--- src/register.js
+++ src/register.js
@@ -1,45 +1,46 @@
-function registerUser() {
-    const name = document.getElementById('first_name').value;
-    const surname = document.getElementById('last_name').value;
-    const email = document.getElementById('email').value;
-    const cell = document.getElementById('cell_num').value;
-    const password = document.getElementById('password').value;
-
+async function registerUser() {
+    const name = document.getElementById("first_name").value;
+    const surname = document.getElementById("last_name").value;
+    const email = document.getElementById("email").value;
+    const cell = document.getElementById("cell_num").value;
+    const password = document.getElementById("password").value;
+  
+    if (
+      typeof(name) === "number" ||
+      typeof(surname) === "number"){
+      return alert(`Please use the correct values for each section!`);
+    }
+      
     // console.log(name, surname, email, cell, password)
     try {
-        if (typeof(name) === "number" || 
-        typeof(surname) === "number" || 
-        typeof(cell) === "string") {
-            throw ('Please use the correct values for each section!');
-        }
-    } catch (e) {
-        alert("Error: " + e)
-    } finally {
-    fetch('https://lca-pointofsales.herokuapp.com//register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
+      const res = await fetch(
+        "https://lca-pointofsales.herokuapp.com//register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
             first_name: name,
             last_name: surname,
             email: email,
             cell_num: cell,
-            password: password
-        })
-    }).then(res => res.json()).then(data => {
-        console.log(data)
-        console.log("You have succesfully registered")
-
-        if (data['message'] == 'Success') {
-            alert("Please Sign In On Next Page")
-            window.location.href = "./login.html"
-        } else {
-            alert('Please FIll In The Required Fields Correctly')
+            password: password,
+          }),
         }
-    })
+      );
+      const json = await res.json();
+      console.log("You have succesfully registered");
+      if (json["message"] == "Success"){
+        alert("Please Sign In On Next Page");
+        window.location.href = "./login.html";
+      } else {
+        alert("Please Fill In The Required Fields Correctly");
+      }
+    } catch (err) {
+      console.error(err);
     }
-}
+  }
 
 // try {
 //     if (typeof name !== 'string' && typeof surname !== 'string' && typeof cell !== 'number') {
--- src/view-products.js
+++ src/view-products.js
@@ -3,7 +3,8 @@ let base_URL = "https://lca-pointofsales.herokuapp.com//view-products";
 let cartProduct = [];
 
 let cart = [];
-console.log(cart);
+
+let products;
 
 // view product function
 function getProducts(url) {
@@ -12,57 +13,145 @@ function getProducts(url) {
     .then((data) => {
       console.log(data);
 
-      cartProduct = data.data;
+      cartProduct = data;
 
-      console.log(cartProduct);
+      // console.log(cartProduct);
 
-      let products = data.data;
+      products = data.data;
 
-      // console.log(products)
-
-      let container = document.querySelector("#view-products");
-
-      container.innerHTML = "";
-
-      products.forEach((product) => {
-        container.innerHTML += `<div class='products-container'>
-          <img src='${product[5]}' class='productImage'></img>
-          <div class="productID">Product ID: ${product[0]}</div>
-          <div class='productName'>Product Name: ${product[1]}</div>
-          <div class='productCategory'>Category: ${product[2]}</div>
-          <div class='productPrice'>Price: R${product[3]}</div>
-          <div class="productDescription"><p>Product Description:</p> ${product[4]}</div>
-          <div class="button" ><button class="btn" onclick="addProduct(${product[0]})">Add To Cart</button> <input type="number" id="quantity" name="quantity" min="1" max="5"></div>`;
-      });
+      renderProducts(products);
     });
 }
 
 getProducts(base_URL);
 
+function renderProducts(products) {
+  let container = document.querySelector("#view-products");
+
+  container.innerHTML = "";
+
+  products.forEach((product) => {
+    container.innerHTML += `<div class='products-container'>
+      <img src='${product[5]}' class='productImage'></img>
+      <div class="productID">Product ID: ${product[0]}</div>
+      <div class='productName'>Product Name: ${product[1]}</div>
+      <div class='productCategory'>Category: ${product[2]}</div>
+      <div class='productPrice'>Price: R${product[3]}</div>
+      <div class="productDescription"><p>Product Description:</p> ${product[4]}</div>
+      <div class="button" ><button class="btn" onclick="addProduct(${product[0]})">Add To Cart</button></div>`;
+  });
+}
+
+function createCart(cartItems) {
+  let container = document.querySelector('#cart');
+
+  container.innerHTML = "";
+
+  if (cartItems.length > 0) {
+    cartItems.map(cartItem => {
+      container.innerHTML += `<div class='products-container'>
+      <img src='${cartItem[5]}' class='productImage'></img>
+      <div class="productID">Product ID: ${cartItem[0]}</div>
+      <div class='productName'>Product Name: ${cartItem[1]}</div>
+      <div class='productCategory'>Category: ${cartItem[2]}</div>
+      <div class='productPrice'>Price: R${cartItem[3]}</div>
+      <div class="productDescription"><p>Product Description:</p> ${cartItem[4]}</div>
+      <button id="remove-item">Remove Item</button></div>`;
+    })
+    
+    let accumulatedPrice = cartItems.reduce((total, item) => total + item[3], 0)
+    
+    container.innerHTML += `<h2>Total Price:</h2> ${ accumulatedPrice }`
+
+    let remove = document.getElementById('remove-item')
+
+    remove.addEventListener('click', (id) => {
+      let product = cartProduct.data.find((item) => {
+        return item[0] == id;
+      });
+      
+      console.log(product)
+      cart.pop(product);
+      console.log(cart)
+    
+      let lesserPrice = cartItems.reduce((total, item) => total - item[3], 0)
+      
+      container.innerHTML = '';
+      
+      container.innerHTML += `<h2>Total Price:</h2> ${ lesserPrice }`
+    })
+  } else {
+    container.innerHTML = "<h2>No Items In Cart</h2>";
+  }
+}
+
 function addProduct(id) {
-  let product = cartProduct.find((item) => {
+  let product = cartProduct.data.find((item) => {
     return item[0] == id;
   });
   console.log(product);
   cart.push(product);
   console.log(cart);
   // console.log("hello");
+  createCart(cart)
 }
 
 function searchProducts() {
-  let searchTerm = document.getElementById('searchTerm').value;
+  let searchTerm = document.querySelector('#searchTerm').value;
 
-  // console.log(searchTerm)
+  // console.log(cartProduct)
 
-  let searchedItems = cartProduct.filter((item) => {
-     item[0].toLowerCase().includes(searchTerm.toLowerCase())
+  console.log(searchTerm)
+
+  let searchedItems = cartProduct.data.filter((item) => {
+    return item[1].toLowerCase().includes(searchTerm.toLowerCase());
   })
+  
   console.log(searchedItems)
+  // console.log(cartProduct)
 
   if (searchedItems.length == 0) {
     document.querySelector("#view-products").innerHTML = "<h2>There Are No Products of that Description</h2>"
   } else {
-    getProducts(searchedItems)
+    renderProducts(searchedItems)
   }
 
+}
+
+function sortAscending() {
+  let ascendingProducts = cartProduct.data.sort((a, b) => {
+    if (a[1] > b[1]) return 1;
+    if (a[1] < b[1]) return -1;
+    return 0;
+  })
+  renderProducts(ascendingProducts);
+}
+
+function sortDescending() {
+  let ascendingProducts = cartProduct.data.sort((a, b) => {
+    if (a[1] > b[1]) return 1;
+    if (a[1] < b[1]) return -1;
+    return 0;
+  })
+
+  ascendingProducts.reverse();
+  renderProducts(ascendingProducts);
+}
+
+function sortPriceAscending() {
+  let sortedProducts = products.sort((a, b) => a[3] - b[3]).reverse();
+  renderProducts(sortedProducts)
+}
+
+function sortPriceDescending() {
+  let sortedProducts = products.sort((a, b) => a[3] - b[3]);
+  renderProducts(sortedProducts)
+}
+
+function clearFilter() {
+  getProducts(base_URL)
+}
+
+function toggleCart() {
+  document.querySelector('#cart').classList.toggle('active');
 }
